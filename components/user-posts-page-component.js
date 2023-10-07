239fa6e76e6b0294fef937e115a7260157a89fd8
Andruha99 <Andriei.korovin.2014@mail.ru>
2023-10-07 21:29:10 +0300

    feat: add likes in user posts list

--- components/user-posts-page-component.js
+++ components/user-posts-page-component.js
@@ -1,10 +1,11 @@
 import { USER_POSTS_PAGE } from "../routes.js";
 import { renderHeaderComponent } from "./header-component.js";
-import { posts, goToPage } from "../index.js";
+import { posts, goToPage, getToken } from "../index.js";
+import { addLike, deleteLike } from "../api.js";
 
 export function renderUserPostsComponent({ appEl }) {
   const postsHtml = posts
-    .map((post) => {
+    .map((post, index) => {
       return `<li class="post">
         <div class="post-header" data-user-id="${post.user.id}">
            <img src="${post.user.imageUrl}" class="post-header__user-image">
@@ -14,12 +15,17 @@ export function renderUserPostsComponent({ appEl }) {
           <img class="post-image" src="${post.imageUrl}">
         </div>
         <div class="post-likes">
-          <button data-post-id="${post.id}" class="like-button">
-             ${
-               post.isLiked
-                 ? '<img src="./assets/images/like-active.svg">'
-                 : '<img src="./assets/images/like-not-active.svg">'
-             }
+          <button data-index=${index} data-post-id="${
+        post.id
+      }" data-post-isLiked=${post.isLiked} data-user-id=${
+        post.user.id
+      } class="like-button">
+            ${
+              post.isLiked
+                ? '<img src="./assets/images/like-active.svg">'
+                : '<img src="./assets/images/like-not-active.svg">'
+            }
+            
           </button>
           <p class="post-likes-text">
             Нравится: <strong>${post.likes.length}</strong>
@@ -57,6 +63,52 @@ export function renderUserPostsComponent({ appEl }) {
       goToPage(USER_POSTS_PAGE, {
         userId: userEl.dataset.userId,
       });
+    });
+  }
+
+  const likesBtn = document.querySelectorAll(".like-button");
+  console.log(likesBtn);
+
+  for (let likeBtn of likesBtn) {
+    likeBtn.addEventListener("click", () => {
+      console.log(likeBtn.dataset);
+      console.log(typeof likeBtn.dataset.postIsliked);
+      if (likeBtn.dataset.postIsliked === "false") {
+        addLike({ postId: likeBtn.dataset.postId, token: getToken() }).then(
+          (response) => {
+            // likeBtn.dataset.isLiked = true;
+            goToPage(USER_POSTS_PAGE, {
+              userId: likeBtn.dataset.userId,
+            });
+            console.log(response);
+          }
+        );
+      } else {
+        deleteLike({ postId: likeBtn.dataset.postId, token: getToken() }).then(
+          (response) => {
+            // likeBtn.dataset.isLiked = true;
+            goToPage(USER_POSTS_PAGE, {
+              userId: likeBtn.dataset.userId,
+            });
+            console.log(response);
+          }
+        );
+      }
+      // if (likeBtn.dataset.isliked === "true") {
+      //   deleteLike({ postId: likeBtn.dataset.postId, token: getToken() }).then(
+      //     () => {
+      //       // likeBtn.dataset.isLiked = false;
+      //       // goToPage(POSTS_PAGE);
+      //     }
+      //   );
+      // } else {
+      //   addLike({ postId: likeBtn.dataset.postId, token: getToken() }).then(
+      //     () => {
+      //       // likeBtn.dataset.isLiked = true;
+      //       // goToPage(POSTS_PAGE);
+      //     }
+      //   );
+      // }
     });
   }
 }
